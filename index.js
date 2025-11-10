0e08f90487e464f39ce4956fe9030e8fe948cd88
ajhar-07 <fahimajhar737@gmail.com>
2025-11-11 01:41:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require('express')
+require('dotenv').config()
+const cors=require('cors')
+const port=process.env.PORT || 3000;
+// const admin = require("firebase-admin");
+
+// const serviceAccount = require("");
+
+const app=express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+app.use(cors())
+app.use(express.json())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufivfja.mongodb.net/?appName=Cluster0`;
+app.get('/', (req,res)=>{
+    res.send("Welcome to Surver")
+})
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db=client.db('FinEaseDB')
+const TransactionCollection=db.collection('Transactions')
+const MoneyCollection =db.collection("Money");
+
+// const bidCollection=db.collection('bids')
+// const usersCollection = db.collection('users')
+
+    
+// add transaction API
+app.post('/add-transaction', async(req,res)=>{
+  const NewTransaction=req.body
+  const result=await TransactionCollection.insertOne(NewTransaction)
+  res.send(result)
+})
+
+app.get('/add-transaction',  async(req,res)=>{
+  const email=req.query.email
+  query={}
+  if(email){
+    query.email=email
+  }
+  const cursor=TransactionCollection.find(query).sort({amount:-1})
+  const result=await cursor.toArray()
+  res.send(result)
+})
+
+
+app.get('/transactions/:id', async(req,res)=>{
+  const id=req.params.id
+  const quary={_id: new ObjectId(id)}
+  const result= await TransactionCollection.findOne(quary)
+  res.send(result)
+})
+
+app.delete('/transactions/:id', async(req,res)=>{
+  const id=req.params.id
+  const quary={_id:new ObjectId(id)}
+  const result=await TransactionCollection.deleteOne(quary)
+  res.send(result)
+})
+
+
+app.patch('/transactions/:id', async (req,res)=>{
+       const id=req.params.id
+       const UpdateTransaction=req.body
+    const quary={_id:new ObjectId(id)}
+    const update={
+        $set:{
+          
+        type: UpdateTransaction.type,
+        category: UpdateTransaction.category,
+        amount: UpdateTransaction.amount,
+        description: UpdateTransaction.description,
+        date: UpdateTransaction.date,
+        email: UpdateTransaction.email,
+        name: UpdateTransaction.name,
+      
+        }
+    }
+    const result= await TransactionCollection.updateOne(quary,update)
+    res.send(result)
+})
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`port is running on - ${port}`);
+    
+})
+
+
